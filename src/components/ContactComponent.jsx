b30f9b8a996f026f6af79e55bbf44503431835d5
VTVIVEK <vivek78643@gmail.com>
2020-05-09 19:54:21 +0530

    Uncontrolled Form

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -77,6 +77,8 @@ class Contact extends Component{
 
     }
 
+  
+
     handleSubmit(event) {
         console.log('Current State is: ' + JSON.stringify(this.state));
         alert('Current State is: ' + JSON.stringify(this.state));
